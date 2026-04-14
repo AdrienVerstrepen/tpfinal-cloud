cf007c9ad15c584c75ba951d4aac9baa573b8bdf
Adrien Verstrepen <adrienverstrepen@gmail.com>
2026-04-14 12:15:10 +0100

    Ajout du boilerplate vue

---
+++ tp-final-adrien-verstrepen/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  const buildId = import.meta.env.VITE_BUILD_ID ?? 'local'
+  const today = new Date().toLocaleDateString('fr-FR')
+</script>
+<template>
+  <header>
+    <p>Nom : Adrien Verstrepen</p>
+    <p>Date : {{ today }}</p>
+    <p>Build : {{ buildId }}</p>
+  </header>
+</template>
